6d5985248f1697d8e6da383d5c5070b05bcd5d07
lalithkiranp <112470472+lalithkiranp@users.noreply.github.com>
2026-04-19 16:16:37 +0530

    Print DB_URL from environment variables
    checking the injection of environment variables.

--- src/main/java/com/example/surveyapp/SurveyappApplication.java
+++ src/main/java/com/example/surveyapp/SurveyappApplication.java
@@ -9,6 +9,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class SurveyappApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SurveyappApplication.class, args);
+		System.out.println("DB_URL=" + System.getenv("DB_URL"));
 	}
 
 }
